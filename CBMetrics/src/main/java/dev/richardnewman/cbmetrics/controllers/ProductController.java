505bac265192bd72645141864549309f36edfc7d
Richard Newman <17536735+richardanewman@users.noreply.github.com>
2020-02-23 00:40:02 -0700

    adds product controller and service impl

---
+++ CBMetrics/src/main/java/dev/richardnewman/cbmetrics/controllers/ProductController.java
@@ -0,0 +1,75 @@
+package dev.richardnewman.cbmetrics.controllers;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.richardnewman.cbmetrics.entities.Product;
+import dev.richardnewman.cbmetrics.services.ProductService;
+
+@RestController
+@RequestMapping("api")
+public class ProductController {
+	
+	@Autowired
+	private ProductService prodSvc;
+	
+	@GetMapping("products")
+	private List<Product> index() {
+		return prodSvc.index();
+	}
+	
+	@GetMapping("product/{id}")
+	private Product getProductById(@PathVariable int id) {
+		return prodSvc.show(id);
+	}
+	
+	@PostMapping("product")
+	private Product createProduct(
+			@RequestBody Product product,
+			HttpServletRequest req,
+			HttpServletResponse res) {
+		
+		try {
+			product = prodSvc.create(product);
+			res.setStatus(200);
+			StringBuffer url = req.getRequestURL();
+			url.append("/").append(product.getId());
+			res.addHeader("Location", url.toString());
+			return product;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			res.setStatus(400);
+			return null;
+		}
+	}
+	
+	@PutMapping("product/{id}")
+	private Product updateProduct(
+			@PathVariable int id,
+			@RequestBody Product product,
+			HttpServletResponse res) {
+		
+		try {
+			product = prodSvc.update(id, product);
+			res.setStatus(200);
+			return product;
+		} catch (Exception e) {
+			e.printStackTrace();
+			res.setStatus(400);
+			return null;
+		}
+	}
+
+}
--- CBMetrics/src/main/java/dev/richardnewman/cbmetrics/services/ProductServiceImpl.java
+++ CBMetrics/src/main/java/dev/richardnewman/cbmetrics/services/ProductServiceImpl.java
@@ -1,5 +1,65 @@
 package dev.richardnewman.cbmetrics.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import dev.richardnewman.cbmetrics.entities.Product;
+import dev.richardnewman.cbmetrics.repositories.ProductRepository;
+
+@Service
 public class ProductServiceImpl implements ProductService {
+	
+	@Autowired
+	private ProductRepository prodRepo;
+
+	@Override
+	public List<Product> index() {
+		return prodRepo.findAll();
+	}
+
+	@Override
+	public Product show(int id) {
+		Product prod = null;
+		Optional<Product> opt = prodRepo.findById(id);
+		if (opt.isPresent()) {
+			prod = opt.get();
+			return prod;
+		}
+		return null;
+	}
+
+	@Override
+	public Product create(Product product) {
+		return prodRepo.saveAndFlush(product);
+	}
+
+	@Override
+	public Product update(int id, Product product) {
+		Product update = null;
+		Optional<Product> opt = prodRepo.findById(id);
+		if (opt.isPresent()) {
+			update = opt.get();
+		}
+		update.setActiveDate(product.getActiveDate());
+		update.setAvgEarningsPerSale(product.getAvgEarningsPerSale());
+		update.setCategories(product.getCategories());
+		update.setCbID(product.getCbID());
+		update.setCommission(product.getCommission());
+		update.setDescription(product.getDescription());
+		update.setGravity(product.getGravity());
+		update.setHasRecurringProducts(product.isHasRecurringProducts());
+		update.setInitialEarningsPerSale(product.getInitialEarningsPerSale());
+		update.setPercentPerRebill(product.getPercentPerRebill());
+		update.setPercentPerSale(product.getPercentPerSale());
+		update.setPopularityRank(product.getPopularityRank());
+		update.setReferred(product.getReferred());
+		update.setTitle(product.getTitle());
+		update.setTotalRebillAmt(product.getTotalRebillAmt());
+		prodRepo.saveAndFlush(update);
+		return update;
+	}
 
 }
