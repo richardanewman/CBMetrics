94f1d9a2d6e2afed05937f2331cdeb90def0f54d
Richard Newman <17536735+richardanewman@users.noreply.github.com>
2020-02-23 13:23:25 -0700

    creates initial earnings controller and completes GET mappings

--- CBMetrics/src/main/java/dev/richardnewman/cbmetrics/services/InitialEarningsPerSaleService.java
+++ CBMetrics/src/main/java/dev/richardnewman/cbmetrics/services/InitialEarningsPerSaleService.java
@@ -1,5 +1,15 @@
 package dev.richardnewman.cbmetrics.services;
 
+import java.util.List;
+
+import dev.richardnewman.cbmetrics.entities.InitialEarningsPerSale;
+
 public interface InitialEarningsPerSaleService {
+	
+public List<InitialEarningsPerSale> index();
+	
+	public List<InitialEarningsPerSale> showByProductID(int pid); //Get All by Product ID (pid)
+	
+	public InitialEarningsPerSale show(int id);
 
 }
