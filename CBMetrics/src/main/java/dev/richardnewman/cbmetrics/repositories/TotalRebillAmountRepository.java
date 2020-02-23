169fedc503acb87e08a321e3e04776c0668882b4
Richard Newman <17536735+richardanewman@users.noreply.github.com>
2020-02-23 14:46:22 -0700

    creates total rebill amount controller and maps GET routes

--- CBMetrics/src/main/java/dev/richardnewman/cbmetrics/repositories/TotalRebillAmountRepository.java
+++ CBMetrics/src/main/java/dev/richardnewman/cbmetrics/repositories/TotalRebillAmountRepository.java
@@ -1,9 +1,13 @@
 package dev.richardnewman.cbmetrics.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import dev.richardnewman.cbmetrics.entities.TotalRebillAmount;
 
 public interface TotalRebillAmountRepository extends JpaRepository<TotalRebillAmount, Integer> {
+	
+	public List<TotalRebillAmount> findByProduct_Id(int pid);
 
 }
