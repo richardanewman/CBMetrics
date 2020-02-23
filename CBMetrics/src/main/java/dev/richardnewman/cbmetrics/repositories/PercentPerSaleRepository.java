ac39775061b70f119f0ae0266114b52f8579efbb
Richard Newman <17536735+richardanewman@users.noreply.github.com>
2020-02-23 13:46:38 -0700

    creates percent per sale controller and maps GET routes

--- CBMetrics/src/main/java/dev/richardnewman/cbmetrics/repositories/PercentPerSaleRepository.java
+++ CBMetrics/src/main/java/dev/richardnewman/cbmetrics/repositories/PercentPerSaleRepository.java
@@ -1,9 +1,13 @@
 package dev.richardnewman.cbmetrics.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import dev.richardnewman.cbmetrics.entities.PercentPerSale;
 
 public interface PercentPerSaleRepository extends JpaRepository<PercentPerSale, Integer> {
+	
+	public List<PercentPerSale> findByProduct_Id(int pid);
 
 }
--- CBMetrics/src/main/java/dev/richardnewman/cbmetrics/services/PercentPerSaleServiceImpl.java
+++ CBMetrics/src/main/java/dev/richardnewman/cbmetrics/services/PercentPerSaleServiceImpl.java
@@ -1,5 +1,32 @@
 package dev.richardnewman.cbmetrics.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import dev.richardnewman.cbmetrics.entities.PercentPerSale;
+import dev.richardnewman.cbmetrics.repositories.PercentPerSaleRepository;
+
+@Service
 public class PercentPerSaleServiceImpl implements PercentPerSaleService {
+	
+	@Autowired
+	private PercentPerSaleRepository ppsRepo;
+
+	@Override
+	public List<PercentPerSale> index() {
+		return ppsRepo.findAll();
+	}
+
+	@Override
+	public List<PercentPerSale> showByProductID(int pid) {
+		return ppsRepo.findByProduct_Id(pid);
+	}
+
+	@Override
+	public PercentPerSale show(int id) {
+		return ppsRepo.findById(id).get();
+	}
 
 }
