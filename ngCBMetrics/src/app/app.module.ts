f501a8bf1cca1eae484fef31ccd6caf0bd5d5479
Richard Newman <17536735+richardanewman@users.noreply.github.com>
2020-02-23 23:26:03 -0700

    creates home, login, logout, navbar, notfound, register, and user-account components and configures login and logout function'

--- ngCBMetrics/src/app/app.module.ts
+++ ngCBMetrics/src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { UserAccountComponent } from './components/user-account/user-account.component';
+import { HomeComponent } from './components/home/home.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { LogoutComponent } from './components/logout/logout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    NotFoundComponent,
+    LoginComponent,
+    RegisterComponent,
+    UserAccountComponent,
+    HomeComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
